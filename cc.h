4c22bd73737b2c6eeb0b19c5386beb07ec63ff82
Andrew Chambers <andrewchamberss@gmail.com>
2015-06-13 03:21:40 +1200

    work

--- cc.h
+++ cc.h
@@ -49,9 +49,20 @@ typedef struct {
 } Node;
 
 /* TODO: implement enum, then swap. */
-#define TOKIF     1001
+#define TOKIF     1001 /* TODO: appropriate start point */
 #define	TOKDO     1002
 #define	TOKFOR    1003
 #define	TOKWHILE  1004
 #define TOKRETURN 1005
 #define TOKEOF    1006
+#define TOKPOSINC 1007
+#define TOKADDASS 1008
+#define TOKPOSDEC 1009
+#define TOKSUBASS 1010
+#define TOKMULASS 1011
+#define TOKDIVASS 1012
+#define TOKMODASS 1013
+#define TOKGTEQL  1014
+#define TOKLTEQL  1015
+#define TOKNEQL   1016
+#define TOKEQL    1017
