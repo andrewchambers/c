588de7e5ec4d399e9ef290c3d688e84f56866342
Andrew Chambers <andrewchamberss@gmail.com>
2015-06-26 17:34:30 +1200

    work

--- test.sh
+++
@@ -1,9 +0,0 @@
-set -e
-mkdir -p ./test/
-for CFILE in *.c
-do
-    echo "int __builtin_frame_address(int);" > ./test/$CFILE
-    tcc -D'__asm__(x)=' -E $CFILE | grep -v "^#" >> ./test/$CFILE
-    echo ./test/$CFILE
-    ./c ./test/$CFILE
-done
