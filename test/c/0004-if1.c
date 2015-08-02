f138e2ad5005b56f22a913a7c1fb4364dd72c543
Andrew Chambers <andrewchamberss@gmail.com>
2015-08-03 11:11:01 +1200

    next failing test

---
+++ test/c/0004-if1.c
@@ -0,0 +1,19 @@
+int
+main() {
+	if(0) {
+		return 1;
+	} else if(0) {
+		/* pass */	
+	} else {
+		if(1) {
+			if(0)
+				return 1;
+			else
+				return 0;
+		} else {
+			return 1;
+		}
+	}
+	return 1;
+}
+
