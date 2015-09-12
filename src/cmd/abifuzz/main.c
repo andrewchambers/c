da2f9f7eb0f94abc5b31d14478646a893730fcbb
Andrew Chambers <andrewchamberss@gmail.com>
2015-09-12 22:32:41 +1200

    Add more types to abifuzz

--- src/cmd/abifuzz/main.c
+++ src/cmd/abifuzz/main.c
@@ -4,46 +4,72 @@
 #include <time.h>
 
 #define MAXPARAMS  10
-#define MAXSTRUCTM 6
-#define MAXNEST 3
+#define MAXSTRUCTM 10
+#define MAXNEST 4
 
-enum {
+typedef enum {
 	CHAR,
+	SHORT,
 	INT,
+	LONG,
+	LLONG,
 	STRUCT,
 	TEND,
 } Type;
 
 typedef struct {
-	int t;
+	Type t;
 	union {
 		struct {
 			char v;
 		} Cchar;
 		struct {
+			short v;
+		} Cshort;
+		struct {
 			int v;
 		} Cint;
+		struct {
+			long v;
+		} Clong;
+		struct {
+			long long v;
+		} Cllong;
 		struct {
 			char *name;
 			Vec  *members;
 		} Cstruct;
 	};
-} Param;
+} Val;
 
-static Param *randparam();
+typedef struct {
+	Val *ret;
+	Vec *vals;
+} Testcase;
+
+static Val *randval();
 
 void
-printparamtype(Param *p)
+printvaltype(Val *v)
 {
-	switch(p->t) {
+	switch(v->t) {
 	case CHAR:
 		printf("char");
+		break;
+	case SHORT:
+		printf("short");
 		break;
 	case INT:
 		printf("int");
 		break;
+	case LONG:
+		printf("long");
+		break;
+	case LLONG:
+		printf("long long");
+		break;
 	case STRUCT:
-		printf("struct %s", p->Cstruct.name);
+		printf("struct %s", v->Cstruct.name);
 		break;
 	default:
 		panic("internal error");
@@ -52,48 +78,63 @@ printparamtype(Param *p)
 
 int structcount = 0;
 
-static Param *
+static Val *
 randstruct(int depth)
 {
-	Param *r, *p;
+	Val *r, *p;
 	int i, n; 
 	char buff[1024];
 
-	r = gcmalloc(sizeof(Param));
+	r = gcmalloc(sizeof(Val));
 	r->t = STRUCT;
 	r->Cstruct.members = vec();
 	n = rand() % MAXSTRUCTM;
 	for(i = 0; i < n; i++)
-		vecappend(r->Cstruct.members, randparam(depth + 1));
+		vecappend(r->Cstruct.members, randval(depth + 1));
 	snprintf(buff, sizeof buff, "s%d", structcount++);
 	r->Cstruct.name = gcstrdup(buff);
 	printf("struct %s {\n", r->Cstruct.name);
 	for(i = 0; i < r->Cstruct.members->len; i++) {
 		p = vecget(r->Cstruct.members, i);
 		printf("\t");
-		printparamtype(p);
+		printvaltype(p);
 		printf(" m%d;\n", i);
 	}
 	printf("};\n");
 	return r;
 }
 
-static Param *
-randparam(int depth)
+static Val *
+randval(int depth)
 {
-	Param *r;
+	Val *r;
 
 	again:
 	switch(rand() % TEND) {
 	case CHAR:
-		r = gcmalloc(sizeof(Param));
+		r = gcmalloc(sizeof(Val));
 		r->t = CHAR;
 		r->Cchar.v = rand();
-		break;	
+		break;
+	case SHORT:
+		r = gcmalloc(sizeof(Val));
+		r->t = SHORT;
+		r->Cshort.v = rand();
+		break;
 	case INT:
-		r = gcmalloc(sizeof(Param));
+		r = gcmalloc(sizeof(Val));
 		r->t = INT;
 		r->Cint.v = rand();
+		break;
+	case LONG:
+		r = gcmalloc(sizeof(Val));
+		r->t = LONG;
+		r->Clong.v = rand();
+		break;
+	case LLONG:
+		r = gcmalloc(sizeof(Val));
+		r->t = LLONG;
+		r->Cllong.v = rand();
 		break;
 	case STRUCT:
 		if(depth > MAXNEST)
@@ -101,89 +142,48 @@ randparam(int depth)
 		r = randstruct(depth);
 		break;
 	default:
-		panic("internal error randparam");
+		panic("internal error randval");
 	}
 	return r;
 }
 
-static Vec *
-randparams()
+static Testcase *
+randtestcase()
 {
-	Vec *r;
+	Testcase *t;
 	int n, i;
 	
-	r = vec();
+	t = gcmalloc(sizeof(Testcase));
+	t->vals = vec();
+	t->ret = randval(0);
 	n = rand() % MAXPARAMS;
 	for(i = 0; i < n; i++)
-		vecappend(r, randparam(0));
-	return r;
-}
-
-
-static void
-printcheck(char *sel, Param *p)
-{
-	Param *mem;
-	char buf[128];
-	int i;
-
-	switch(p->t) {
-	case CHAR:
-		printf("\tif(%s != %d) return 1;\n", sel, p->Cchar.v);
-		break;
-	case INT:
-		printf("\tif(%s != %d) return 1;\n", sel, p->Cint.v);
-		break;
-	case STRUCT:
-		for(i = 0; i < p->Cstruct.members->len; i++) {
-			mem = vecget(p->Cstruct.members, i);
-			snprintf(buf, sizeof(buf), "%s.m%d", sel, i);
-			printcheck(buf, mem);
-		}
-		break;
-	default:
-		panic("internal error");
-	}
+		vecappend(t->vals, randval(0));
+	return t;
 }
 
 static void
-printfunc(Vec *v)
+printinit(char *sel, Val *p)
 {
-	int i;
-	char buf[64];
-	Param *p;
-
-	printf("int\n");
-	printf("f(");
-	for(i = 0; i < v->len; i++) {
-		p = vecget(v, i);
-		printparamtype(p);
-		printf(" p%d%s", i, (i == v->len - 1) ? "" : ", ");
-	}
-	printf(")\n");
-	printf("{\n");
-	for(i = 0; i < v->len; i++) {
-		p = vecget(v, i);
-		snprintf(buf, sizeof(buf), "p%d", i);
-		printcheck(buf, p);
-	}
-	printf("	return 0;\n");
-	printf("}\n");
-}
-
-static void
-printinit(char *sel, Param *p)
-{
-	Param *mem;
-	char buf[128];
-	int i;
+	Val *mem;
+	char   buf[128];
+	int    i;
 
 	switch(p->t) {
 	case CHAR:
 		printf("\t%s = %d;\n", sel, p->Cchar.v);
 		break;
+	case SHORT:
+		printf("\t%s = %d;\n", sel, p->Cshort.v);
+		break;
 	case INT:
 		printf("\t%s = %d;\n", sel, p->Cint.v);
+		break;
+	case LONG:
+		printf("\t%s = %ld;\n", sel, p->Clong.v);
+		break;
+	case LLONG:
+		printf("\t%s = %lld;\n", sel, p->Cllong.v);
 		break;
 	case STRUCT:
 		for(i = 0; i < p->Cstruct.members->len; i++) {
@@ -198,19 +198,91 @@ printinit(char *sel, Param *p)
 }
 
 static void
-printmain(Vec *v)
+printcheck(char *sel, Val *p)
 {
-	Param *p;
-	char buf[64];
+	Val *mem;
+	char buf[128];
 	int i;
 
+	switch(p->t) {
+	case CHAR:
+		printf("\tif(%s != %d) abort();\n", sel, p->Cchar.v);
+		break;
+	case SHORT:
+		printf("\tif(%s != %d) abort();\n", sel, p->Cshort.v);
+		break;
+	case INT:
+		printf("\tif(%s != %d) abort();\n", sel, p->Cint.v);
+		break;
+	case LONG:
+		printf("\tif(%s != %ld) abort();\n", sel, p->Clong.v);
+		break;
+	case LLONG:
+		printf("\tif(%s != %lld) abort();\n", sel, p->Cllong.v);
+		break;
+	case STRUCT:
+		for(i = 0; i < p->Cstruct.members->len; i++) {
+			mem = vecget(p->Cstruct.members, i);
+			snprintf(buf, sizeof(buf), "%s.m%d", sel, i);
+			printcheck(buf, mem);
+		}
+		break;
+	default:
+		panic("internal error");
+	}
+}
+
+static void
+printfunc(Testcase *t)
+{
+	int i;
+	char buf[64];
+	Vec *v;
+	Val *p;
+
+	v = t->vals;
+	printvaltype(t->ret);
+	printf("\n");
+	printf("f(");
+	for(i = 0; i < v->len; i++) {
+		p = vecget(v, i);
+		printvaltype(p);
+		printf(" p%d%s", i, (i == v->len - 1) ? "" : ", ");
+	}
+	printf(")\n");
+	printf("{\n");
+	printf("\t");
+	printvaltype(t->ret);
+	printf(" r;\n");
+	printinit("r", t->ret);
+	for(i = 0; i < v->len; i++) {
+		p = vecget(v, i);
+		snprintf(buf, sizeof(buf), "p%d", i);
+		printcheck(buf, p);
+	}
+	printf("\treturn r;");
+	printf("}\n");
+}
+
+static void
+printmain(Testcase *t)
+{
+	Vec *v;
+	Val *p;
+	char   buf[64];
+	int    i;
+	
+	v = t->vals;
 	printf("int\n");
 	printf("main()\n");
 	printf("{\n");
+	printf("\t");
+	printvaltype(t->ret);
+	printf(" r;\n");
 	for(i = 0; i < v->len; i++) {
 		p = vecget(v, i);
 		printf("\t");
-		printparamtype(p);
+		printvaltype(p);
 		printf(" p%d;\n", i);
 	}
 	for(i = 0; i < v->len; i++) {
@@ -218,11 +290,13 @@ printmain(Vec *v)
 		snprintf(buf, sizeof(buf), "p%d", i);
 		printinit(buf, p);
 	}
-	printf("\treturn f(");
+	printf("\tr = f(");
 	for(i = 0; i < v->len; i++) {
 		printf("p%d%s", i, (i == v->len - 1) ? "" : ", ");
 	}
 	printf(");\n");
+	printcheck("r", t->ret);
+	printf("\treturn 0;\n");
 	printf("}\n");
 }
 
@@ -249,7 +323,7 @@ getseed()
 
 int main(int argc, char *argv[])
 {
-	Vec *v;
+	Testcase *t;
 	int seed;
 
 	if(argc == 1)
@@ -258,8 +332,9 @@ int main(int argc, char *argv[])
 		seed = strtoul(argv[1], 0, 10);
 	printf("/* seed %u */\n", seed);
 	srand(seed);
-	v = randparams();
-	printfunc(v);
-	printmain(v);
+	t = randtestcase();
+	printf("void abort(void);\n");
+	printfunc(t);
+	printmain(t);
 	return 0;
 }
