0643656e17b5d8b7bd0411bf2718058cae37cdbc
shubhampaliwal2020 <73382942+shubhampaliwal2020@users.noreply.github.com>
2021-10-21 22:35:33 +0530

    Update 0001-define3.c

--- test/cpp/0001-define3.c
+++ test/cpp/0001-define3.c
@@ -1,11 +1,12 @@
 #define A 3
 #define FOO(X,Y,Z) X + Y + Z
 #define SEMI ;
-
+#define Marva 10;
 int
 main()
 {
 	if(FOO(1, 2, A) != 6)
+		printf("Marva %d",Marva);
 		return 1 SEMI
 	return FOO(0,0,0);
 }
